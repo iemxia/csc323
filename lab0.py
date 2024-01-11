81525e031b995b3720fd809dd367c47f44bcfc8b
Xia Webster <153694504+xmw2003@users.noreply.github.com>
2024-01-11 10:19:07 -0800

    init lab 0 and lab 0 test files

--- lab0.py
+++ lab0.py
@@ -1,1 +1,16 @@
+def main():
+    print("hello world")
 
+    def stringBytesToHexASCII(stringofBytes):
+
+
+    def hexASCIItoBytes(hexASCIIstring):
+
+
+    def base64ToBytes(base64String):
+
+    def bytesToBase64(bytes):
+
+
+if __name__ == "__main__":
+    main()
