5ac09a6f8deb267c53dc9f0391d591767be48a37
Xia Webster <xia@edurain.org>
2024-02-13 10:49:20 -0800

    padding and unpadding tests, working

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -4,21 +4,22 @@
 # how to get parts and then change and submit, w3c site, do encoding correctly ('utf-8')
 
 
-def pad(stream, block_size):
-    pad_len = block_size - (len(stream) % block_size)  # get how many padding bytes we'll need
-    return stream + bytes([pad_len] * pad_len)  # add that value as padding to end
+def pad(msg, block_size):
+    pad_len = block_size - (len(msg) % block_size)  # get how many padding bytes we'll need
+    return msg + bytes([pad_len] * pad_len)  # add that value as padding to end of msg
 
 
-def unpad(stream, block_size):
-    if len(stream) % block_size != 0:
+def unpad(msg, block_size):
+    if len(msg) % block_size != 0:
         raise ValueError('stream length invalid, must be multiple of block size')
-
-    for i in range(block_size, 0, -1):
-        guessed_padding = stream[-i:]
-        padding_vals = set(guessed_padding)
-        if len(padding_vals) == 1 and padding_vals.pop() == i:
-            return stream[:-i]
-    raise ValueError('No pad')
-
+    pad_len = msg[-1]  # for a valid padding, the last byte value will be equal to the number of padding bytes
+    # that exist
+    for i in range(1, pad_len + 1):  # go through the msg backwards pad length positions
+        # print(f'Stream byte: {msg[-i]} at pos: {-i}')
+        pos_pad = msg[-i]  # get possible pos pad
+        if pos_pad != pad_len:  # check that the byte is equal to the pad length number
+            raise ValueError('invalid padding')
+    msg = msg[:-i]  # cut the pad byte off
+    return msg
 
 
--- Labs/lab2tests.py
+++ Labs/lab2tests.py
@@ -21,10 +21,28 @@ class MyTestCase(unittest.TestCase):
 		msg = b'Hello'
 		self.assertEqual(unpad(to_unpad, block_size), msg)
 
-	def test_unpadfullmsg(self):
-		to_unpad = b'12345678'
+	def test_unpad_wrongsize(self):
+		to_unpad = b'Hello'
 		block_size = 8
-		msg = b'12345678\x08\x08\x08\x08\x08\x08\x08\x08'
+		with self.assertRaises(ValueError):
+			unpad(to_unpad, block_size)
+
+	def test_unpad_weirdpadding(self):
+		to_unpad = b'Hello\x03\x02\x01'
+		block_size = 8
+		msg = b'Hello\x03\x02'
+		self.assertEqual(unpad(to_unpad, block_size), msg)
+
+	def test_unpadweird(self):
+		to_unpad = b'Hell\x03\x03\x03\x03'
+		block_size = 8
+		msg = b'Hell\x03'
+		self.assertEqual(unpad(to_unpad, block_size), msg)
+
+	def test_unpadfullmsg(self):
+		msg = b'12345678'
+		block_size = 8
+		to_unpad = b'12345678\x08\x08\x08\x08\x08\x08\x08\x08'
 		self.assertEqual(unpad(to_unpad, block_size), msg)
 
 
