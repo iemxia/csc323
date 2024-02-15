24b32ba3629ffd72dedf5bd47e831fd0fef45fb8
iemxia <xmw2003@gmail.com>
2024-02-14 23:59:39 -0800

    implement ECB mode

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -2,7 +2,8 @@
 # create AES object, specify mode (ECB), but only ever encrypt one block at a time
 # never pass it more than one block at a time
 # how to get parts and then change and submit, w3c site, do encoding correctly ('utf-8')
-
+from Crypto.Cipher import AES
+import base64
 
 def pad(msg, block_size):
     pad_len = block_size - (len(msg) % block_size)  # get how many padding bytes we'll need
@@ -23,3 +24,48 @@ def unpad(msg, block_size):
     return msg
 
 
+def ecb_encrypt(key, plaintext):
+    block_size = AES.block_size
+    cipher = AES.new(key, AES.MODE_ECB)
+    padded = pad(plaintext, block_size)
+    ciphertext = cipher.encrypt(padded)
+    return ciphertext
+
+
+def ecb_decrypt(key, ciphertext, pad_scheme):
+    block_size = AES.block_size
+    if len(ciphertext) % block_size != 0:  # not a multiple of the block size
+        raise ValueError("Must be multiple of block size")
+    cipher = AES.new(key, AES.MODE_ECB)
+    decrypted = cipher.decrypt(ciphertext)
+    if pad_scheme == "pkcs7":
+        plaintext = unpad(decrypted, block_size)
+    else:
+        plaintext = ansi_x_unpad(decrypted, block_size)
+    return plaintext
+
+
+def ansi_x_unpad(msg, block_size):
+    pad_len = msg[-1]
+    if pad_len == 0 or pad_len > block_size:
+        raise ValueErorr("Invalid Padding")
+    for i in range(1, pad_len):
+        if msg[-i -1] != 0:
+            raise ValueError("Invalid padding")
+    unpadded = msg[:-pad_len]
+    return unpadded
+
+
+def main():
+    with open("Lab2.TaskII.A.txt", "r") as file:
+        base64_ciphertext = file.read()
+
+    ciphertext = base64.b64decode(base64_ciphertext)
+    key = b'CALIFORNIA LOVE!'
+    plaintext = ecb_decrypt(key, ciphertext, "pkcs7")
+    plaintext_str = plaintext.decode('ascii')
+    print("Decrypted plaintext: ", plaintext_str)
+
+
+if __name__ == "__main__":
+    main()
