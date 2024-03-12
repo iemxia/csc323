deef8bc8dff312474d716bb88eaa930c947ec382
Xia Webster <xia@edurain.org>
2024-03-12 09:49:14 -0700

    addition tests passed

--- Labs/Lab3/Lab3.py
+++ Labs/Lab3/Lab3.py
@@ -1,38 +1,10 @@
 from collections import namedtuple
+from dataclasses import dataclass
 Point = namedtuple("Point", "x y")
-O = 'Origin'
+O = "Origin"
 
 
-def inv_mod_p(x, curve):
-    # Return inverse for x mod curve.field, assuming x not divisble by the field
-    if pow(x, 1, curve.field) == 0:
-        raise ZeroDivisionError("Cannot inverse")
-    return pow(x, curve.field - 2, curve.field)
-
-def ec_inv(P, curve):
-    # Fcn to return inverse of point P on curve
-    if P == O:
-        return P
-    return Point(P.x, pow((-P.y), 1, curve.field))
-
-def ecc_add(P, Q, curve):
-    if P == O:
-        res = Q
-    elif Q == O:
-        res = P
-    elif Q == ec_inv(P):
-        res = O
-    else:
-        # no origin stuff
-        if P == Q:
-            dydx = (3 * P.x ** 2 + curve.a) * inv_mod_p(2 * P.y)
-        else:
-            dydx = (Q.y - P.y) * inv_mod_p(Q.x - P.x)
-        x = pow((dydx ** 2 - P.x - Q.x), 1, curve.field)
-        y = pow((dydx * (P.x - x) - P.y), 1, curve.field)
-        result = Point(x, y)
-    return result
-
+@dataclass(frozen=True)
 class Curve:
     a: int
     b: int
@@ -40,4 +12,31 @@ class Curve:
 
     def __str__(self):
         return f"Curve: Y^2 = X^3 + {self.a}X + {self.b} over field {self.field}"
+
+
+def ec_inv(P, curve):
+    # Fcn to return inverse of point P on curve
+    if P == O:
+        return P
+    return Point(P.x, pow((-P.y), 1, curve.field))
+
+
+def ecc_add(P, Q, curve):
+    if P == O:
+        res = Q
+    elif Q == O:
+        res = P
+    elif Q == ec_inv(P, curve):
+        res = O
+    else:
+        # no origin stuff
+        if P == Q:
+            dydx = (3 * pow(P.x, 2, curve.field) + curve.a) * pow(2 * P.y, -1, curve.field)
+        else:
+            dydx = (Q.y - P.y) * pow(Q.x - P.x, -1, curve.field)
+        dydx = pow(dydx, 1, curve.field)
+        x = pow((pow(dydx, 2, curve.field) - P.x - Q.x), 1, curve.field)
+        y = pow((dydx * (P.x - x) - P.y), 1, curve.field)
+        res = Point(x, y)
+    return res
 
--- Labs/Lab3/lab3test.py
+++ Labs/Lab3/lab3test.py
@@ -1,8 +1,28 @@
 import unittest
+from Lab3 import *
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+	def test_ec_add(self):
+		curve = Curve(a=3, b=8, field=13)
+		P = Point(9, 7)
+		Q = Point(1, 8)
+		R = ecc_add(P, Q, curve)
+		self.assertEqual(R, Point(2, 10))
+
+	def test_ec_add2(self):
+		curve = Curve(a=3, b=8, field=13)
+		P = Point(9, 7)
+		Q = Point(9, 7)
+		R = ecc_add(P, Q, curve)
+		self.assertEqual(R, Point(9, 6))
+
+	def test_ec_add3(self):
+		curve = Curve(a=3, b=8, field=13)
+		P = Point(12, 11)
+		Q = Point(12, 2)
+		R = ecc_add(P, Q, curve)
+		self.assertEqual(R, "Origin")
+
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
